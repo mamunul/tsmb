88328e2a3988f7905bf3297f45e4589c7ea4f571
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-11-13 22:13:24 +0600

    Initial Commit

---
+++ TSMB/TSMBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSMBApp.swift
+//  TSMB
+//
+//  Created by Mamunul Mazid on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TSMBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

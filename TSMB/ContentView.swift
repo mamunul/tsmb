b5e1e65b2234172de48635b4f1a49a79e3c80259
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-11-13 23:25:12 +0600

    add sample code to test

--- TSMB/ContentView.swift
+++ TSMB/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var presenter = SocketNetworkService()
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,11 +17,51 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            presenter.openWebSocket()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class SocketNetworkService: ObservableObject {
+    // https://appspector.com/blog/websockets-in-ios-using-urlsessionwebsockettask
+    // https://jayeshkawli.ghost.io/using-websockets-on-ios-using/
+    func openWebSocket() {
+        let urlString = "https://punch.onlinewebshop.net?client-id=mac&exclude-me"
+        guard let url = URL(string: urlString) else { return }
+        var urlRequest = URLRequest(url: url)
+
+        let session = URLSession(configuration: .default)
+        let webSocket = session.webSocketTask(with: urlRequest)
+
+//        let message = URLSessionWebSocketTask.Message.string("Hello Socket")
+//        webSocket.send(message) { error in
+//            if let error = error {
+//                print("WebSocket sending error: \(error)")
+//            }
+//        }
+
+        webSocket.receive { result in
+            switch result {
+            case let .failure(error):
+                print("Failed to receive message: \(error)")
+            case let .success(message):
+                switch message {
+                case let .string(text):
+                    print("Received text message: \(text)")
+                case let .data(data):
+                    print("Received binary message: \(data)")
+                @unknown default:
+                    fatalError()
+                }
+            }
+        }
+        webSocket.resume()
     }
 }
